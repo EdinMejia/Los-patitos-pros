8b1fffaca96ab47d798c11d29f9e0b9264b91c13
Edin Mejía <edinmejia1208@gmail.com>
2025-06-09 16:55:45 -0600

    update numero 2

---
+++ agenda.java
@@ -0,0 +1,17 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        // Imprime un saludo
+        System.out.println("Hola, mundo en 3d!");
+        System.out.println("Hola, mundo en 4d!");
+
+        // Declara dos números
+        int numero1 = 6;
+        int numero2 = 3;
+
+        // Suma los números
+        int suma = numero1 + numero2;
+
+        // Muestra el resultado
+        System.out.println("La suma de " + numero1 + " y " + numero2 + " es " + suma);
+    }
+}
